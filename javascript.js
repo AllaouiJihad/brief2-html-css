f71d31c98b7b5827264c7338f089932800f3b2e6
AllaouiJihad <allaouijihad05@gmail.com>
2023-10-21 21:45:33 +0200

    add code javascript for menu burger

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
-const menu = document.getElementsByClassName('menu')[0]
-const nav = document.getElementsByClassName('nav')[0]
+// const menu = document.getElementsByClassName('menu');
+// const nav = document.getElementsByClassName('nav');
 
-menu.addEventListener('click', () => {
-nav.classList.toggle('active')
-})
+// menu.addEventListener('click', () => {
+// navbarLinks.classList.toggle('active')
+// })
+
+let menu = document.getElementById('menu');
+let nav = document.getElementById('nav'); // Accédez au premier élément de la collection
+
+menu.addEventListener('click', function () {
+    if (nav.style.display === "none") {
+        nav.style.display = "inline-block";
+    }else{
+        nav.style.display = "none";
+    }
+});
